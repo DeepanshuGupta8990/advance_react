a297dab1a1920f0b3840f1b3f1767582d42510a1
DeepanshuGupta8990 <deepanshugupta899@gmail.com>
2024-01-14 20:10:09 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
-import { LargeAuthorListItem } from './components/authors/LargeListItems';
-import { SmallAuthorListItem } from './components/authors/SmallListItems';
-import { LargeBookListItem } from './components/books/LargeListItems';
+import Modal from './components/Modal';
 import { SmallBookListItem } from './components/books/SmallListItems';
 import RegularList from './components/lists/Regular';
-import { authors } from './data/authors';
 import { books } from './data/books';
 
 function App() {
   return (
     <>
-  <h2>Small authors list</h2>
-  <RegularList items={authors} sourceName='author' ItemComponent={SmallAuthorListItem}/>
-  <hr />
-  <h2>Large authors list</h2>
-  <RegularList items={authors} sourceName='author' ItemComponent={LargeAuthorListItem}/>
-  <hr />
-  <h2>Small books list</h2>
-  <RegularList items={books} sourceName={'book'} ItemComponent={SmallBookListItem}/>
-  <hr />
-  <h2>Large books list</h2>
-  <RegularList items={books} sourceName={'book'} ItemComponent={LargeBookListItem}/>
+     <Modal>
+      <RegularList items={books} sourceName={'book'} ItemComponent={SmallBookListItem}/>
+     </Modal>
     </>
   );
 }
