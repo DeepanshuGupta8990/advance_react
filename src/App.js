a922e090fe882b21d86557f1f4941e803a44ec9a
DeepanshuGupta8990 <deepanshugupta899@gmail.com>
2024-01-22 17:07:14 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import { SplitScreen } from './components/Split_screen';
 
-const LeftSideComp = ({title})=>{
-  return(
-    <h2 style={{background:'red'}}>{title}</h2>
-  )
-}
-
-const RightSideComp = ({title})=>{
-  return(
-    <h2 style={{background:'blue'}}>{title}</h2>
-  )
-}
 
 function App() {
   return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSideComp title='Left side component'/>
-      <RightSideComp title='Right side component'/>
-    </SplitScreen>
+    <h1>Start here</h1>
   );
 }
 
