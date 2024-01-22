fb6aae2ca4c24491a03521db2afc1efe3bfba921
DeepanshuGupta8990 <deepanshugupta899@gmail.com>
2024-01-22 18:47:11 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { UserInfo } from './components/UserInfo';
+import { UserInfo2 } from './components/UserInfo2';
 
 
 function App() {
   return (
-    <h1>Start here</h1>
+     <>
+      <h1>Start here</h1>
+      <UserInfo/>
+      <UserInfo2 userId={'1'}/>
+      <UserInfo2 userId={'2'}/>
+      <UserInfo2 userId={'3'}/>
+     </>
   );
 }
 
---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,25 @@
+import { useCurrentUser } from "./current-user-hook";
+
+export const UserInfo = ()=>{
+    const user = useCurrentUser();
+    const {name,age,country,books} = user || {};
+    return user ? (
+        <>
+          <h3>{name}</h3>
+          <p>Age: {age}</p>
+          <p>country: {country}</p>
+          <p>Books</p>
+          <ul>
+            {
+                books.map((book)=>{
+                    return(
+                        <li key={book}>{book}</li>
+                    )
+                })
+            }
+          </ul>
+        </>
+    ) : (
+        <h1>Loading...</h1>
+    )
+}
